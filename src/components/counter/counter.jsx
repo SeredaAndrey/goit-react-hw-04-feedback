a062d7d0fd5d0a1d160560fefe1a1992154b18b3
Andrii Sereda <a.a.sereda@ukr.net>
2022-12-28 23:02:24 +0200

    add save to localstorage

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,11 +1,38 @@
 import Controls from 'components/controls/controls';
 import Values from 'components/values/values';
-import { useState } from 'react';
+import { useRef } from 'react';
+import { useEffect, useState } from 'react';
+
+const LS_KEY = 'localstorage_key_feedback';
 
 export default function Counter() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    switch (isFirstRender.current) {
+      case true: {
+        const data = JSON.parse(localStorage.getItem(LS_KEY));
+        if (data) {
+          setGood(data.good);
+          setBad(data.bad);
+          setNeutral(data.neutral);
+        }
+        isFirstRender.current = false;
+        break;
+      }
+      case false: {
+        const data = { good, bad, neutral };
+        localStorage.setItem(LS_KEY, JSON.stringify(data));
+        break;
+      }
+
+      default:
+        break;
+    }
+  }, [good, bad, neutral]);
 
   const handleIncrementGood = () => {
     setGood(state => state + 1);
@@ -28,54 +55,3 @@ export default function Counter() {
     </div>
   );
 }
-
-// class Counter extends React.Component {
-//   static defaultProps = {
-//     initialValueGood: 0,
-//     initialValueNeutral: 0,
-//     initialValueBad: 0,
-//   };
-
-//   state = {
-//     good: this.props.initialValueGood,
-//     neutral: this.props.initialValueNeutral,
-//     bad: this.props.initialValueBad,
-//   };
-
-//   handleIncrementGood = () => {
-//     this.setState(prevState => ({
-//       good: prevState.good + 1,
-//     }));
-//   };
-
-//   handleIncrementNeutral = () => {
-//     this.setState(prevState => ({
-//       neutral: prevState.neutral + 1,
-//     }));
-//   };
-
-//   handleIncrementNegative = () => {
-//     this.setState(prevState => ({
-//       bad: prevState.bad + 1,
-//     }));
-//   };
-
-//   render() {
-//     return (
-//   <div className="counter">
-//     <Controls
-//       onIncrementGood={this.handleIncrementGood}
-//       onIncrementNeutral={this.handleIncrementNeutral}
-//       onIncrementBad={this.handleIncrementNegative}
-//     />
-//     <Values
-//       goodValue={this.state.good}
-//       neutralValue={this.state.neutral}
-//       badValue={this.state.bad}
-//     />
-//   </div>
-// );
-//   }
-// }
-
-// export default Counter;
